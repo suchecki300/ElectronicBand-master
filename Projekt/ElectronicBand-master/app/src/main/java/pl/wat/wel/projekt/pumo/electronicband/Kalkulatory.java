b5b1401a7f37c0b0f0bb012384ee7766a55653d9
rev3ng <konsle17@wp.pl>
2017-03-29 21:04:11 +0200

    Repair
    Repairs bad Android Studio project

--- Projekt/ElectronicBand-master/app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/Kalkulatory.java
+++ Projekt/ElectronicBand-master/app/src/main/java/pl/wat/wel/projekt/pumo/electronicband/Kalkulatory.java
@@ -5,7 +5,6 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.LinearLayout;
 
 public class Kalkulatory extends AppCompatActivity {
     Button prosty;
@@ -32,13 +31,6 @@ public class Kalkulatory extends AppCompatActivity {
         logiczne.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                LinearLayout logic_submenu = (LinearLayout) findViewById(R.id.logic_submenu);    //find logic submenu
-                if (logic_submenu.getVisibility() == View.GONE){ //if submenu is OFF
-                    logic_submenu.setVisibility(View.VISIBLE); //turn it ON
-                }
-                else {
-                    logic_submenu.setVisibility(View.GONE); // else, turn OFF
-                }
                // Intent intent = new Intent (getApplicationContext(), Prosty_Kalkulator.class);
             }
         });
